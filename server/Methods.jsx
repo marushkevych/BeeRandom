18cde3c13c4dd41b42468c173fbb080b89ef9495
marushkevych <marushkevych@gmail.com>
2016-01-09 16:33:24 -0500

    random pages

--- server/Methods.jsx
+++ server/Methods.jsx
@@ -16,13 +16,27 @@ Meteor.methods({
 
 });
 
-var index = -1;
 
 
 function getNextBeer(){
-  var result = HTTP.call('GET', 'http://lcboapi.com/products',
-      {params: {store_id: 511, q: 'beer', order: 'id.asc'}});
 
-  index +=1;
-  return result.data.result[index];
+  var pageInfo = getBeers(1);
+
+  var pages = pageInfo.data.pager.total_pages;
+
+  var result = getBeers(getRandomInt(pages));
+
+  return result.data.result[0];
+}
+
+function getBeers(page){
+  var params = {store_id: 511, q: 'beer', order: 'id.asc', page: page};
+  return HTTP.call('GET', 'http://lcboapi.com/products', {params});
+}
+
+/**
+ * Get random between 1 and max inclusive
+ */
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max)) + 1;
 }
